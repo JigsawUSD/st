2373b7e1383a438a6b0edefdebe870f278008597
NINJA GAME -YT <srclash98@gmail.com>
2025-12-22 01:51:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2998259438-c083e",
+  "appId": "1:612397565194:web:e6728258c088dc79bb26f3",
+  "apiKey": "AIzaSyCpG9BS-65HCNqUF1Hv3Cflb58eERL_ehc",
+  "authDomain": "studio-2998259438-c083e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "612397565194"
+};
