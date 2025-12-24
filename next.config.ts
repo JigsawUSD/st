60533906ce374f5afc6835a3c63f438883b43886
NINJA GAME -YT <srclash98@gmail.com>
2025-12-24 15:56:18 +0000

    imfelismente acho que perdi o site pq ainda esta aparecendo erro 404 e n

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-
-  // Adicionado para compatibilidade com GitHub Pages
-  basePath: '/st',
-  assetPrefix: '/st/',
   
   typescript: {
     ignoreBuildErrors: true,
