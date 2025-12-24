6d6aa9189a7b2eaa0a6c7eef31fc07226668dbb6
NINJA GAME -YT <srclash98@gmail.com>
2025-12-24 15:58:02 +0000

    sim pode fazer ent

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type {NextConfig} from 'next';
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   
+  basePath: isGithubPages ? '/st' : undefined,
+  assetPrefix: isGithubPages ? '/st/' : undefined,
+
   typescript: {
     ignoreBuildErrors: true,
   },
